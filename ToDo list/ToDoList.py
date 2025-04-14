3d47a672a43f15ef44f78d025ad157db5e0759d2
Anjani911 <anjani10610@gmail.com>
2025-04-14 22:25:17 +0530

    updated to user input

--- ToDo list/ToDoList.py
+++ ToDo list/ToDoList.py
@@ -6,4 +6,26 @@ def display_menu():
     print("4. Delete Task")
     print("5. Exit")
 
+def main():
+    tasks = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice (1-5): ")
+        
+        if choice == '1':
+            print("Viewing tasks...")
+        elif choice == '2':
+            print("Adding a task...")
+        elif choice == '3':
+            print("Marking a task as done...")
+        elif choice == '4':
+            print("Deleting a task...")
+        elif choice == '5':
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice. Please enter 1-5.")
+
+if __name__ == "__main__":
+    main()
 
